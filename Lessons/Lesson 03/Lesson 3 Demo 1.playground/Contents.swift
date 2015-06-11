b910071ae3e5e182e7c54c91a54b5632da21d88f
hdebegon <handy_debgon@yahoo.com>
2015-06-10 21:27:59 -0400

    June 10th work

---
+++ Lessons/Lesson 03/Lesson 3 Demo 1.playground/Contents.swift
@@ -0,0 +1,38 @@
+// My first program
+let Title = "Hello, World!"
+
+// let is a keyword. It means constant.
+
+// My second program
+var day = "Wednesday"
+day = "Tuesday"
+
+// var is a keyword. It means variable
+
+
+// I am explicitly setting a constant named 'age' to an Integer that is equal to 28
+let age: Int = 28
+let implicitAge = 28
+
+let balance: Float = 10.99
+let implicitBalance = 10.99
+
+// float takes decimals
+// interger does not take decimals
+
+let gender: Bool = true
+let implicitGender = true
+
+let name: String = "Handy"
+let implicitName = "Handy"
+
+// string is anything between 2 double quotes
+
+
+/*
+    This is the real deal
+    sdfsfsfsfsf
+*/
+
+// immutable means that you CANNOT change its value
+// mutable means that you can change its value
---
+++ Lessons/Lesson 03/Lesson 3 Demo 4.playground/Contents.swift
@@ -0,0 +1,31 @@
+/*let x = 0
+
+if x > 0 {
+    println("Yes, \(x) is greater than zero")
+} else {
+    println("No, \(x) is less than zero")
+}*/
+
+let y = 0
+
+if y > 0 {
+    println("Yes, \(y) is greater than zero")
+} else if y == 0 {
+    println("WOOHOO, \(y) is equal to zero!")
+} else {
+    println("No, \(y) is less than zero")
+}
+
+var z = 0
+while z < 100 {
+    println("Hey! \(z)")
+    ++z
+}
+
+
+// highlight, then commnd (/? button) will automatically comment a section
+
+// var i = 1
+for i in 1...100 {
+  println("Print the current iteration: \(toyota)")
+}
