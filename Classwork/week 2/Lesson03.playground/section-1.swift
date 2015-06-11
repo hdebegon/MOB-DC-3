5890298c3d6bf3b205042f9d4568255ab5f342b5
hdebegon <handy_debgon@yahoo.com>
2015-06-10 21:32:01 -0400

    new work from Arthur

--- Classwork/week 2/Lesson03.playground/section-1.swift
+++ Classwork/week 2/Lesson03.playground/section-1.swift
@@ -3,38 +3,11 @@
 import Foundation
 
 // TODO: Create two variables, name and age. Name is a string, age is an integer.
+
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
+
+
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
-
-var name = "Handy"
-var age = 28
-
-println("My name is \(name) and I am \(age) years old")
-
-let x = age
-
-if x > 21 {
-println("I can drink")
-}
-
-let y = age
-
-if y > 16 && y < 18 {
-    println("I can drive")
-}
-
-if y > 18 && y < 21 {
-    println("I can drive and vote")
-}
-if y > 21 {
-    println("I can drive, vote an drink")
-}
-
-else if y == 0 {
-    println("WOOHOO, \(y) is equal to zero!")
-} else {
-    println("No, \(y) is less than zero")
-}
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
 
@@ -52,3 +25,15 @@ else if y == 0 {
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
 // BONUS TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
+
+// 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+let x = 10
+
+if x < 20 && x > 10 {
+    println("x < 20")
+} else if x <= 10 {
+    println("x < 10")
+}
+
