350c52c3dbd19249d9d1aba0477cd54ce3391a77
hdebegon <handy_debgon@yahoo.com>
2015-07-23 23:18:06 -0400

    idk

--- Classwork/Property List/Property List/MoviesAndMusicTableViewController.swift
+++ Classwork/Property List/Property List/MoviesAndMusicTableViewController.swift
@@ -8,90 +8,52 @@
 
 import UIKit
 
-class MoviesAndMusicTableViewController: UITableViewController {
+class MoviesAndMusicTableViewController: UITableViewController
+{
 
+    var movies = [String]()
+    var songs = [String]()
+    let cellID = "hdID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        tableView.registerClass(UITableView.self, forHeaderFooterViewReuseIdentifier: cellID)
+        movies = ["Kris", "Rashod", "Roy", "Dylan"]
+        songs = ["Apple", "Microsoft", "Google"]
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
+    
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        
+        return 2
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        
+        let rows = (section == 0) ? movies.count : songs.count
+        
+        return rows
     }
 
-    /*
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return (section == 0) ? "movies" : "songs"
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellID, forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = (indexPath == 0) ? movies[indexPath.row] : songs[indexPath.row]
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
+    
+
+
+
+
--- Classwork/Property List/Property List/ViewController.swift
+++ Classwork/Property List/Property List/ViewController.swift
@@ -1,55 +1,31 @@
-//
-//  ViewController.swift
-//  Property List
-//
-//  Created by user on 7/22/15.
-//  Copyright (c) 2015 Handy Debegon. All rights reserved.
-//
+////
+////  ViewController.swift
+////  Property List
+////
+////  Created by user on 7/22/15.
+////  Copyright (c) 2015 Handy Debegon. All rights reserved.
+////
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var movies = []
 
-
-//    let counterID = "counterID"
-//    let defaults = NSUserDefaults.standardUserDefaults()
-//    
-//    var counter = 0 {
-//        willSet {
-//            println("Previous Value \(counter)")
-//        }
-//        
-//        didSet {
-//            println("New Value \(counter)")
-//            movieNames.text = "\(counter)"
-//            
-//            defaults.setInteger(counter, forKey: counterID)
-//            defaults.synchronize()
-//        }
-//    }
-//    
-//    @IBOutlet weak var movieNames: UITextField!
-//
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        counter = defaults.integerForKey(counterID)
-//    }
-//    
-//    @IBAction func buttonAction(sender: AnyObject) {
-//        counter++
-//    }
+    @IBOutlet weak var movieNames: UITextField!
     
-
-    let favoriteSongs = NSBundle.mainBundle().pathForResource("Songs", ofType: "plist")
     
-    if let favoriteSongs = favoriteSongs {
-        let songs = NSArray(contentsOfFile: favoriteSongs)
-        println(songs)
-    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        var movies = movieNames.text
+    }
+
+    @IBAction func saveAction(sender: UIButton) {
     
+        var movies = movieNames.text
+    
+    }
 
+    
 }
-
